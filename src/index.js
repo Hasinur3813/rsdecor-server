16ff3afd4021d116a1500385aa08497daef076f0
Hasinur <hasinur3813@gmail.com>
2026-06-30 09:30:53 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -54,12 +54,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
-    );
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
 
 module.exports = app;
